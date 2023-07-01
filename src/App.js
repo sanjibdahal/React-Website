d3dcf3ce388da3ac492c4bca9fd5445ad13a7759
Sanjib Dahal <sanjibdahal04@gmail.com>
2023-07-01 16:56:55 +0545

    simple

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Namaste from Sanjib Dahal <br/> from one of the most beautiful country <h1>Nepal</h1>.
+          Namaste from Sanjib Dahal <br/> from one of the most beautiful country <h1>Nepal</h1>
+          hello hello hello hello
 
         </p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aut iste itaque maiores ut! Architecto at aut dolor dolores esse est molestiae nemo, provident quos voluptas. Accusantium corporis dolorem dolorum incidunt magni minima numquam sed voluptate. Ab animi asperiores deleniti, dolor eligendi eos est excepturi explicabo fugiat hic, illo ipsam minima nemo nulla officia placeat quasi sed similique sint ut. Accusantium architecto commodi distinctio dolorum earum eligendi expedita illum in itaque iure, iusto minima neque numquam odio omnis perferendis perspiciatis provident quaerat veniam voluptas. Adipisci aperiam asperiores assumenda consequuntur cum debitis dicta dignissimos dolor ducimus error eveniet expedita hic illo, mollitia nemo non praesentium quasi quo sequi soluta tempore ut veritatis voluptas! Asperiores doloremque illo, saepe tenetur veniam veritatis voluptates!
+        
         <a
           className="App-link"
           href="https://reactjs.org"
